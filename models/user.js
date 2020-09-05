55a3982b80c55a4b8b70eb1d37fffcd47bceba7b
mittalprince <princemittal766@gmail.com>
2020-09-05 19:23:20 +0530

    Add User Schema

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    email:{
+        required: true,
+        type: String,
+        unique: true,
+        trim: true
+    },
+    name:{
+        required:true,
+        type: String,
+
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    followers:[{
+        follwerId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    }],
+    followings:[{
+        followingPersonId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    }],
+    posts:[{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Posts'
+    }]
+})
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
