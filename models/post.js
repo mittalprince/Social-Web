2d2edd0c96d552a5ede0fe779ecc523f36384e68
mittalprince <princemittal766@gmail.com>
2020-09-05 19:32:28 +0530

    Add Post's Schema

---
+++ models/post.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const PostSchema = new mongoose.Schema({
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    topic:{
+        type: String,
+        required: true
+    },
+    message:{
+        type: String,
+        required: true
+    },
+    comments:[{
+        text:{
+            type: String,
+            required: true
+        },
+        replies:[{
+            text:{
+                type: String,
+                required: true
+            },
+            likes:{
+                type: Number,
+                default: 0
+            }
+        }],
+        likes:{
+            type: Number,
+            default: 0
+        }
+    }],
+    likes:{
+        type: Number,
+        default: 0
+    }
+})
+
+const Post = mongoose.model('Post', PostSchema);
+module.exports = Post;
